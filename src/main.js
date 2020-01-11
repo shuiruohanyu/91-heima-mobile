1f50458933e093daa3824fe01851790a4f6d66fb
gaoly <shuiruohanyu@foxmail.com>
2020-01-11 11:08:41 +0800

    使用vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant' // 引入 vant组件库
+import 'vant/lib/index.css' // 样式文件
+Vue.use(Vant) // 一旦完成注册  在任意位置就可以使用 Vant组件库的组件
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>主页</div>
+  <div>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,4 @@ export default {
 </script>
 
 <style>
-
 </style>
