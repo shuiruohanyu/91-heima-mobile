035c0fad94cba4fcadd473cfb3465ff6d0327b47
gaoly <shuiruohanyu@foxmail.com>
2020-01-11 18:12:11 +0800

    忘记提交的store

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as auth from '@/utils/auth'
 Vue.use(Vuex)
-// vuex  和缓存数据的同步 redux
+// vuex    和缓存数据的同步 redux
 export default new Vuex.Store({
   // 放置数据的地方  初始化的时候直接将用户信息给我们的公共状态
   state: {
