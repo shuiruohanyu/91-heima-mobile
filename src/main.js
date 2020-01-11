f915666d52e0b9d815f4398086729f3b2a829a8a
gaoly <shuiruohanyu@foxmail.com>
2020-01-11 15:17:06 +0800

    引入全局样式覆盖vant

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant' // 引入 vant组件库
-import 'vant/lib/index.css' // 样式文件
+import 'vant/lib/index.css' // vant 样式文件
+import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
 import 'amfe-flexible' // 引入自动适配插件
 Vue.use(Vant) // 一旦完成注册  在任意位置就可以使用 Vant组件库的组件
 Vue.config.productionTip = false
