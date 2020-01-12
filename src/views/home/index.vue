8db7d691dac84cabfc16394b19dfe66987e0ae17
gaoly <shuiruohanyu@foxmail.com>
2020-01-12 15:27:34 +0800

    使用tabs组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,87 @@
 <template>
-  <div class='container'>
-      主页
+  <div class="container">
+    <van-tabs v-model="activeIndex" swipeable>
+      <van-tab :title="'标签' +  item" v-for="item in 10" :key="item">
+        <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
+        <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
+        <div class='scroll-wrapper'>
+          <van-cell-group>
+            <van-cell v-for="obj in 20" :key="obj" :title="item"></van-cell>
+          </van-cell-group>
+        </div>
+      </van-tab>
+    </van-tabs>
+    <span class="bar_btn">
+      <van-icon name="wap-nav" />
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home' // devtools查看组件时  可以看到 对应的name名称
+  name: 'home', // devtools查看组件时  可以看到 对应的name名称
+  data () {
+    return {
+      activeIndex: 0 // 默认启动第0 个标签
+    }
+  }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content {
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane {
+    height: 100%;
+    .scroll-wrapper {
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
