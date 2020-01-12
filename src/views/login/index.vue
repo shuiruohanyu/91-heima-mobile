8b9197877de0cce07362157cb6e34e74715cda22
gaoly <shuiruohanyu@foxmail.com>
2020-01-12 11:34:53 +0800

    登录基本布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class='container'>
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <!-- 手机号  验证码  登录按钮 -->
+    <van-cell-group>
+      <van-field  label="手机号" placeholder="请输入手机号"></van-field>
+      <van-field  label="验证码" placeholder="请输入验证码">
+        <!-- slot指定给哪个坑填内容 -->
+        <van-button  slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class='btn-box'>
+      <van-button type="info" size="small" round block>登录</van-button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +21,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+   .btn-box {
+     padding: 20px;
+   }
 </style>
