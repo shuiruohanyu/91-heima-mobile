1e0e9b8c760013382133014fc81a8780dde10a62
gaoly <shuiruohanyu@foxmail.com>
2020-01-14 12:30:36 +0800

    时间格式处理

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,9 +1,14 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn' // 引入中文的语言包
+dayjs.extend(relativeTime)
 export default {
   // Vue.use(obj) => 会调用obj 中的install方法
   install (Vue) {
     //   插件
     Vue.prototype.$gnotify = (params) => Vue.prototype.$notify({ duration: 800, ...params }) // 小伎俩
     Vue.prototype.$sleep = sleep // 封装一个休眠函数
+    Vue.filter('relTime', relTime) // 注册一个全局相对时间的过滤器
   }
 }
 // function(time = 500) 如果传time用传的 如果不传 用500
@@ -11,4 +16,11 @@ function sleep (time = 500) {
   return new Promise(function (resolve) {
     setTimeout(() => resolve(), time) // 表示 等待time时间之后 才能resolve
   })
+}
+// 相对时间的过滤器 得到一个相对时间
+/**
+ * value值我们认为就是时间
+ * ****/
+function relTime (value) {
+  return dayjs().locale('zh-cn').from(value)
 }
--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -22,7 +22,8 @@
             <div class="info_box">
               <span>{{ article.aut_name }}</span>
               <span>{{ article.comm_count }}评论</span>
-              <span>{{ article.pubdate }}</span>
+              <!-- 使用过滤器  表达式 | 过滤器名称 -->
+              <span>{{ article.pubdate | relTime}}</span>
               <span class="close">
                 <van-icon name="cross"></van-icon>
               </span>
