7157e69640ff0ad4297a8d548d223228dd090a8a
gaoly <shuiruohanyu@foxmail.com>
2020-01-14 11:48:48 +0800

    完成列表内容的动态渲染

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -6,23 +6,23 @@
       <!-- 放置list组件list组件可以实现 上拉加载 -->
       <van-list v-model="upLoading" :finished="finished" finished-text="没有了" @load="onLoad">
         <!-- v-for 渲染数据 -->
-        <van-cell v-for="article in articles" :key="article">
+        <van-cell v-for="article in articles" :key="article.art_id.toString()">
           <div class="article_item">
-            <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+            <h3 class="van-ellipsis">{{ article.title }}</h3>
             <!-- 三图模式 -->
-            <div class="img_box">
-              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            <div class="img_box" v-if="article.cover.type === 3">
+              <van-image class="w33" fit="cover" :src="article.cover.images[0]" />
+              <van-image class="w33" fit="cover" :src="article.cover.images[1]" />
+              <van-image class="w33" fit="cover" :src="article.cover.images[2]" />
             </div>
             <!-- 单图模式 -->
-            <div class="img_box">
-              <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            <div class="img_box" v-if="article.cover.type === 1">
+              <van-image class="w100" fit="cover" :src="article.cover.images[0]" />
             </div>
             <div class="info_box">
-              <span>你像一阵风</span>
-              <span>8评论</span>
-              <span>10分钟前</span>
+              <span>{{ article.aut_name }}</span>
+              <span>{{ article.comm_count }}评论</span>
+              <span>{{ article.pubdate }}</span>
               <span class="close">
                 <van-icon name="cross"></van-icon>
               </span>
@@ -76,6 +76,7 @@ export default {
       // 请求数据  注意如果 时间戳为就传当前时间
       const data = await getArticles({ channel_id: this.channel_id, timestamp: this.timestamp || Date.now() })
       this.articles.push(...data.results)
+
       // 关掉加载的状态
       this.upLoading = false // 关掉状态
       // 判断历史时间戳 如果你有历史 表示我还可以继续往下看 否则就不看了
