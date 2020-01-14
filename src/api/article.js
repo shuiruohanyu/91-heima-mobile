87b8b52a476de38e3d9eda9fe07ff385cb9a2df3
gaoly <shuiruohanyu@foxmail.com>
2020-01-14 10:59:41 +0800

    封装渲染列表的方法

---
+++ src/api/article.js
@@ -0,0 +1,14 @@
+// 用于文章数据的获取
+
+import request from '@/utils/request'
+/**
+ * 获取推荐文章的数据
+ * axios中 query参数放置在 params上
+ * body参数放置在data上
+ * **/
+export function getArticles (params) {
+  return request({
+    url: 'http://ttapi.research.itcast.cn/app/v1_1/articles',
+    params: { with_top: 1, ...params }
+  })
+}
