d5f2d95a3e4921b8bc931035c7f3c9e9f6004e95
gaoly <shuiruohanyu@foxmail.com>
2020-01-14 10:06:28 +0800

    article-列表文章布局

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -6,7 +6,29 @@
       <!-- 放置list组件list组件可以实现 上拉加载 -->
       <van-list v-model="upLoading" :finished="finished" finished-text="没有了" @load="onLoad">
         <!-- v-for 渲染数据 -->
-        <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+        <van-cell v-for="article in articles" :key="article">
+          <div class="article_item">
+            <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+            <!-- 三图模式 -->
+            <div class="img_box">
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </div>
+            <!-- 单图模式 -->
+            <div class="img_box">
+              <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </div>
+            <div class="info_box">
+              <span>你像一阵风</span>
+              <span>8评论</span>
+              <span>10分钟前</span>
+              <span class="close">
+                <van-icon name="cross"></van-icon>
+              </span>
+            </div>
+          </div>
+        </van-cell>
       </van-list>
     </van-pull-refresh>
   </div>
@@ -48,7 +70,7 @@ export default {
       // 触发下拉刷新
       console.log('下拉刷新')
       setTimeout(() => {
-        let arr = Array.from(Array(10), (value, index) => ('追加' + (index + 1)))
+        let arr = Array.from(Array(10), (value, index) => '追加' + (index + 1))
         this.articles.unshift(...arr) // 将数据添加到队首
         this.downLoading = false // 关掉下拉状态
         this.refreshSuccessText = `更新了${arr.length}条数据`
@@ -58,5 +80,45 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
