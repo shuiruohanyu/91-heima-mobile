94f0ae384e16fc89b86af7cd95e3fab3043078fc
gaoly <shuiruohanyu@foxmail.com>
2020-01-16 11:13:31 +0800

    文章详情-基础布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class='container'>
-    <van-nav-bar title="文章详情" left-arrow @click-left="$router.back()"></van-nav-bar>
+ <div class='container'>
+    <van-nav-bar fixed title="文章详情" left-arrow @click-left="$router.back()" />
+    <div class="detail">
+      <h3 class="title">文章的标题</h3>
+      <div class="author">
+        <van-image round width="1rem" height="1rem" fit="fill" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <div class="text">
+          <p class="name">一阵清风</p>
+          <p class="time">两周内</p>
+        </div>
+        <van-button round size="small" type="info">+ 关注</van-button>
+      </div>
+      <div class="content">
+        <p>文章的内容</p>
+      </div>
+      <div class="zan">
+        <van-button round size="small" class="active" plain icon="like-o">点赞</van-button>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <van-button round size="small" plain icon="delete">不喜欢</van-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +29,56 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container {
+  height: 100%;
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+.detail {
+  padding: 46px 10px 44px;
+  .title {
+    font-size: 18px;
+    line-height: 2;
+  }
+  .zan{
+    text-align: center;
+    padding: 10px 0;
+    .active{
+      border-color:red;
+      color: red;
+    }
+  }
+  .author {
+    padding: 10px 0;
+    display: flex;
+    .text {
+      flex: 1;
+      padding-left: 10px;
+      line-height: 1.5;
+      .name {
+        font-size: 14px;
+        margin: 0;
+      }
+      .time {
+        margin: 0;
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+  .content {
+    padding: 20px 0;
+    overflow: hidden;
+    white-space: pre-wrap;
+    word-break: break-all;
+    /deep/ img{
+      max-width:100%;
+      background: #f9f9f9;
+    }
+    /deep/ code{
+      white-space: pre-wrap;
+    }
+  }
+}
 </style>
