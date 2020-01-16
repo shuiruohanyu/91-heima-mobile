561d7325813eac24a18e86c1d060e2f85ed81986
gaoly <shuiruohanyu@foxmail.com>
2020-01-16 10:36:24 +0800

    搜索结果-基础布局

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -1,6 +1,29 @@
 <template>
   <div class='container'>
-     <van-nav-bar title="搜索结果" left-arrow  @click-left="$router.back()"></van-nav-bar>
+     <van-nav-bar fixed title="搜索结果" left-arrow @click-left="$router.back()" />
+
+      <van-list>
+        <van-cell-group>
+          <van-cell>
+            <div class="article_item">
+              <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+              <div class="img_box">
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+              </div>
+            </div>
+          </van-cell>
+        </van-cell-group>
+      </van-list>
   </div>
 </template>
 
@@ -10,6 +33,37 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container {
+  padding-top: 46px;
+  height: 100%;
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      height: 180px;
+      width: 100%;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    span {
+      padding-right: 10px;
+    }
+  }
+}
 </style>
