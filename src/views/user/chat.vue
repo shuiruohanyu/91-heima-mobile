4c7f1000fb23ac41ca2415e5cd29859099d46b50
gaoly <shuiruohanyu@foxmail.com>
2020-02-07 10:08:52 +0800

    个人中心-小智同学-基础布局

--- src/views/user/chat.vue
+++ src/views/user/chat.vue
@@ -1,15 +1,120 @@
 <template>
-  <div class='container'>
-       <van-nav-bar title="小智同学" left-arrow @click-left="$router.back()"></van-nav-bar>
+<!-- 小智同学的页面结构 -->
+ <div class="container">
+    <van-nav-bar fixed left-arrow @click-left="$router.back()" title="小智同学"></van-nav-bar>
+    <div class="chat-list">
+      <div class="chat-item left">
+        <van-image fit="cover" round src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <div class="chat-pao">ewqewq</div>
+      </div>
+      <div class="chat-item right">
+        <div class="chat-pao">ewqewq</div>
+        <van-image  fit="cover" round src="https://img.yzcdn.cn/vant/cat.jpeg" />
+      </div>
+    </div>
+    <div class="reply-container van-hairline--top">
+      <van-field v-model="value" placeholder="说点什么...">
+        <van-loading v-if="commentLoading" slot="button" type="spinner" size="16px"></van-loading>
+        <span v-else @click="send()" slot="button" style="font-size:12px;color:#999">提交</span>
+      </van-field>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'chat'
+  data () {
+    return {
+      value: '',
+      loading: false
+    }
+  },
+  methods: {
+    send () {
+
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  box-sizing: border-box;
+  background:#fafafa;
+  padding: 46px 0 50px 0;
+  .chat-list {
+    height: 100%;
+    overflow-y: scroll;
+    .chat-item{
+      padding: 10px;
+      .van-image{
+        vertical-align: top;
+        width: 40px;
+        height: 40px;
+      }
+      .chat-pao{
+        vertical-align: top;
+        display: inline-block;
+        min-width: 40px;
+        max-width: 70%;
+        min-height: 40px;
+        line-height: 38px;
+        border: 0.5px solid #c2d9ea;
+        border-radius: 4px;
+        position: relative;
+        padding: 0 10px;
+        background-color: #e0effb;
+        word-break: break-all;
+        font-size: 14px;
+        color: #333;
+        &::before{
+          content: "";
+          width: 10px;
+          height: 10px;
+          position: absolute;
+          top: 12px;
+          border-top:0.5px solid #c2d9ea;
+          border-right:0.5px solid #c2d9ea;
+          background: #e0effb;
+        }
+      }
+    }
+  }
+}
+.chat-item.right{
+  text-align: right;
+  .chat-pao{
+    margin-left: 0;
+    margin-right: 15px;
+    &::before{
+      right: -6px;
+      transform: rotate(45deg);
+    }
+  }
+}
+.chat-item.left{
+  text-align: left;
+  .chat-pao{
+    margin-left: 15px;
+    margin-right: 0;
+    &::before{
+      left: -5px;
+      transform: rotate(-135deg);
+    }
+  }
+}
+.reply-container {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  height: 44px;
+  width: 100%;
+  background: #f5f5f5;
+  z-index: 9999;
+}
 </style>
