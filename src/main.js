12f2814e1cd533a790207c8715ddbe493bf0b2f7
gaoly <shuiruohanyu@foxmail.com>
2020-02-13 15:43:03 +0800

    解决app中物理返回按键的问题

--- src/main.js
+++ src/main.js
@@ -17,3 +17,35 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// 处理 物理返回键的问题
+// 在main.js中添加如下代码  此代码是采用的 5+ Runtime中的plus对象
+document.addEventListener('plusready', function () {
+  var webview = window.plus.webview.currentWebview()
+  window.plus.key.addEventListener('backbutton', function () {
+    webview.canBack(function (e) {
+      if (e.canBack) {
+        webview.back()
+      } else {
+        // webview.close(); //hide,quit
+        // plus.runtime.quit();
+        // 首页返回键处理
+        // 处理逻辑：1秒内，连续两次按返回键，则退出应用；
+        var first = null
+        window.plus.key.addEventListener('backbutton', function () {
+          // 首次按键，提示‘再按一次退出应用’
+          if (!first) {
+            first = new Date().getTime()
+            setTimeout(function () {
+              first = null
+            }, 1000)
+          } else {
+            if (new Date().getTime() - first < 1500) {
+              window.plus.runtime.quit()
+            }
+          }
+        }, false)
+      }
+    })
+  })
+})
